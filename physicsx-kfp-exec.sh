4547ed18ddd61efe29f67801647d00e5fb8e72d1
Danilo Horta <danilo.horta@physicsx.ai>
2025-12-05 11:09:21 +0000

    save

--- physicsx-kfp-exec.sh
+++ physicsx-kfp-exec.sh
@@ -12,7 +12,10 @@ component_name="$2"
 input_file_path="$3"
 output_file_path="$4"
 
-uvx physicsx-component-host --with "$component_distribution" -- \
+uvx physicsx-component-host \
+  --with "$component_distribution" -- \
+  --with "$component_distribution" -- \
+  --with "$component_distribution" -- \
   --component-name "$component_name" \
   --input-file-path "$input_file_path" \
   --output-file-path "$output_file_path"
